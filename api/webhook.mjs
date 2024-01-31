3cc9a45b14a2ce6aac25eb136eda407e75215db8
solomon stephen <33923021+realrufans@users.noreply.github.com>
2024-01-31 18:14:42 +0100

    duration upgraded

--- api/webhook.mjs
+++ api/webhook.mjs
@@ -1,9 +1,13 @@
 import { setWebhookCallback } from "vercel-grammy";
 import { bot } from "../src/bot.mjs";
 
-export const maxDuration = 100;
 // Handler to set webhook url based on request headers
 export default setWebhookCallback(bot, {
   path: "api/update",
   onError: "return",
 });
+
+// Set the maxDuration for the serverless function
+export const config = {
+  maxDuration: 100,
+};
